615e9a9090401eec6335623e6e8aa58f435c264b
colinbruna <brunacbaldin@gmail.com>
2022-10-11 11:01:56 -0300

    deserialização

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+]
+-src/main/java/com/brunas/ecommerce/Order.java,a\9\a96b2c556259fd24649ad8719f209b6e4e13c2de
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+f
+6src/main/java/com/brunas/ecommerce/GsonSerializer.java,7\6\7639f24be949cc213b93a2f7ed0f339aab73877f
+g
+7src/main/java/com/brunas/ecommerce/KafkaDispatcher.java,1\c\1cd7a406d281dace38bea265331594c3fc43dd92
+h
+8src/main/java/com/brunas/ecommerce/ConsumerFunction.java,e\a\eae560d2e49213faefc830b35ca693edc412e79a
+h
+8src/main/java/com/brunas/ecommerce/GsonDeserializer.java,b\6\b62ee8a19cdf25c6a10e9972f04d218a00894567
--- src/main/java/com/brunas/ecommerce/KafkaService.java
+++ src/main/java/com/brunas/ecommerce/KafkaService.java
@@ -5,28 +5,30 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.io.Closeable;
-import java.io.IOException;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
-public class KafkaService implements Closeable {
+public class KafkaService<T> implements Closeable {
 
-    private final KafkaConsumer<String, String> consumer;
+    private final KafkaConsumer<String, T> consumer;
     private final ConsumerFunction parse;
 
-    public KafkaService(String groupId, String topic, ConsumerFunction parse) {
-        this.parse = parse;
-        this.consumer = new KafkaConsumer<String, String>(properties(groupId));
+    KafkaService(String groupId, String topic, ConsumerFunction parse, Class<T> type) {
+        this(parse, groupId, type);
         consumer.subscribe(Collections.singletonList(topic));
     }
 
-    public KafkaService(String groupId, Pattern topic, ConsumerFunction parse) {
-        this.parse = parse;
-        this.consumer = new KafkaConsumer<String, String>(properties(groupId));
+    KafkaService(String groupId, Pattern topic, ConsumerFunction parse, Class<T> type) {
+        this(parse, groupId, type);
         consumer.subscribe(topic);
+    }
+
+    private KafkaService(ConsumerFunction parse, String groupId, Class<T> type) {
+        this.parse = parse;
+        this.consumer = new KafkaConsumer<>(properties(type, groupId));
     }
 
     public void run() {
@@ -43,14 +45,15 @@ public class KafkaService implements Closeable {
         }
     }
 
-    private static Properties properties(String groupId){
+    private Properties properties(Class<T> type, String groupId){
         var properties=new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GsonDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId); //meu serviço terá esse grupo ("default"
         properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
         properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+        properties.setProperty(GsonDeserializer.TYPE_CONFIG, type.getName());
         return properties;
     }
 
